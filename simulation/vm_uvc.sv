b02fc5c804ec182ec30c51f1d97f4ba05c927b0c
ogvalt <ogvalt@gmail.com>
2016-12-06 21:41:09 +0200

    [VM_VERIFICATION] Add base transaction, vending machine transaction and driver classes

--- simulation/vm_uvc.sv
+++ simulation/vm_uvc.sv
@@ -1,9 +1,7 @@
 /*
+ *	File: vm_uvc.sv
  *
- *
- *
- *
- *
+ *	This file contains UVC description
  *
  *
 */
@@ -26,3 +24,59 @@ class base_vm_transaction;
 endclass : base_vm_transaction
 
 `endif // __VM_UVC_DEVICE__
+
+`ifndef __VM_TRANSACTION__
+`define __VM_TRANSACTION__
+
+class vm_transaction extends base_vm_transaction;
+
+	rand logic [3:0] money;
+	rand logic [3:0] product_code;
+
+	constraint c_money_code {
+		money inside {[1:15]};
+	}
+
+	constraint c_product_code {
+		product_code inside {[1:8]}
+	}
+
+	function new();
+	
+	endfunction : new
+
+endclass
+
+`endif // __VM_TRANSACTION__
+
+`ifndef __VM_DRIVER__
+`define __VM_DRIVER__
+
+class vm_driver;
+
+	virtual dut_interface 			dut_port;
+	virtual vm_interface.drv_port	vm_drv_port;
+
+	mailbox #(base_vm_transaction) 	trn_mlb;
+
+	semaphore 						trn_done;
+
+	base_vm_transaction				base_trn;
+	vm_transaction 					vm_trn;
+
+	function new();
+		
+
+		fork
+			this.run_driver();
+		join_none
+	endfunction : new
+
+	task run_driver();
+	
+	endtask : run_driver
+
+
+endclass : vm_uvc
+
+`endif //__VM_DRIVER__
